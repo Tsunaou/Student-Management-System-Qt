4ecd6936340ced25e541e3c80c3bcbb03583e18c
Young <895254752@qq.com>
2018-12-10 18:54:03 +0800

    增加了Students_MS.qss 支持（蓝色主题）

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    StyleSelector::setStyle(":/qss/stylesheet/studentMS.qss");
-//    StyleSelector::setStyle("H:/Advanced Cpp Programming/StudentMS_Qt/stylesheet/studentMS.css");
+//    StyleSelector::setStyle(":/qss/stylesheet/studentMS.qss");
+//    StyleSelector::setStyle("H:/Advanced Cpp Programming/StudentMS_Qt/stylesheet/studentMS.qss");
 
     StudentMS w;
     w.show();
